2d750558f712e2a46ca77962209a54e9c7e9e7a6
FabiTanu <fabihaakter1999@gmail.com>
2023-01-16 12:44:50 +0600

    day 6 completed

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -12,6 +12,7 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -52,9 +53,7 @@ class _LoginPageState extends State<LoginPage> {
                       ),
                       onChanged: (value) {
                         name = value;
-                        setState(() {
-                          
-                        });
+                        setState(() {});
                       },
                     ),
                     TextFormField(
@@ -63,33 +62,47 @@ class _LoginPageState extends State<LoginPage> {
                         hintText: "Enter Password:",
                         labelText: "Password",
                       ),
-                      
                     ),
                     SizedBox(
                       height: 20.0,
                     ),
 
                     InkWell(
-                      onTap: (){
+                      onTap: () async {
+                        setState(() {
+                          changeButton = true;
+                        });
+
+                        await Future.delayed(Duration(seconds: 1));
                         Navigator.pushNamed(context, MyRoutes.homeRoute);
                       },
-                      child: Container(
-                        width: 150,
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changeButton ? 50 : 150,
                         height: 50,
                         //color: Colors.deepPurple,
                         alignment: Alignment.center,
                         // ignore: sort_child_properties_last
-                        child: Text(
-                          "Login",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18.0,
-                          ),
-                        ),
+                        child: changeButton
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18.0,
+                                ),
+                              ),
                         decoration: BoxDecoration(
                           color: Colors.deepPurple,
-                          borderRadius: BorderRadius.circular(8),
+                          // shape: changeButton
+                          // ? BoxShape.circle
+                          // : BoxShape.rectangle ,
+                          borderRadius:
+                              BorderRadius.circular(changeButton ? 50 : 8),
                         ),
                       ),
                     ),
@@ -109,7 +122,6 @@ class _LoginPageState extends State<LoginPage> {
         ));
   }
 }
-
 
 /**child: Center(
         child: Text("Login Page",
